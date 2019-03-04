1e38f490e4135be70794b019a1a19d33befa5bc7
Grant Wilhelm <grant.c.wilhelm@gmail.com>
2019-03-04 15:54:19 -0700

    little practice project folder

---
+++ project/src/MainView.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import TodoItem from './TodoItem';
+
+function MainView() {
+    return (
+        <div>
+            <TodoItem/>
+            <TodoItem/>
+            <TodoItem/>
+            <TodoItem/>
+        </div>
+    )
+}
+
+export default MainView
---
+++ project/src/TodoItem.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function TodoItem() {
+  return (
+    <div>
+      <input type="checkbox" />
+      <p>Lorem ipsum dolor sit amet.</p>
+    </div>
+  );
+}
+
+export default TodoItem;
